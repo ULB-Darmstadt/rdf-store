9b31077d4650b2f0addf12a820b587a4f7e24c9a
Stephan Tittel <stephan.tittel@tu-darmstadt.de>
2025-11-07 11:18:23 +0100

    add readme branch

--- frontend/src/styles.ts
+++
@@ -1,4 +0,0 @@
-import { unsafeCSS } from 'lit'
-import materialStyles from './styles.material.css?inline'
-
-export const globalStyles = unsafeCSS(materialStyles)
--- frontend/vite.config.js
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from "vite";
-
-export default defineConfig({
-    // use relative base path to enable hosting this app on a subpath
-    base: "./",
-});
