0f2d70aa9dbdeeac64d284e8f55c15c1e9a12b16
Stephan Tittel <stephan.tittel@tu-darmstadt.de>
2026-01-27 13:15:11 +0100

    add rebuild function to cli

--- backend/cli/main.go
+++ backend/cli/main.go
@@ -18,10 +18,28 @@ func main() {
 	case "reindex":
 		rdf.ParseAllProfiles()
 		search.Reindex()
+	case "rebuild":
+		rdf.ParseAllProfiles()
+		rebuildResourceMeta()
 	case "sync":
 		profilesync.Synchronize()
 	default:
 		fmt.Println("unknown command", os.Args[1])
 		os.Exit(-1)
+	}
+}
+
+func rebuildResourceMeta() {
+	resourceIds, err := rdf.GetAllResourceIds()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	for _, resourceId := range resourceIds {
+		fmt.Println("update resource meta for", resourceId)
+		_, _, err := rdf.UpdateResourceMetadata(resourceId)
+		if err != nil {
+			fmt.Println(err)
+		}
 	}
 }
